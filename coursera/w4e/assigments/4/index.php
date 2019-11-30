c4c352e0b0ef57a50ce19d01d3325908f4606be8
Oswaldo Nickel <oswaldo.nickel@gmail.com>
2019-11-30 16:06:21 +0100

    add as4

---
+++ coursera/w4e/assigments/4/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Oswaldo Nickel PHP</title>
+</head>
+<body>
+  <h1><pre>O</pre>swaldo Nickel PHP</title>
+<</h1>
+<?php
+  $i = 42;
+  var_dump($i);
+  echo "-------\n\n$i"
+?>
+</body>
+</html>
