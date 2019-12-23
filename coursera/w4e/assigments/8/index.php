cc0c47c7410d4857b20506c0fb27ead4299450d6
Oswaldo Nickel <oswaldo.nickel@gmail.com>
2019-12-23 15:13:25 +0100

    Completed a8

---
+++ coursera/w4e/assigments/8/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Oswaldo Nickel - Rock Paper Scissors</title>
+    <link rel="stylesheet" href="css/style.css" media="all">
+</head>
+<body>
+    <div id="main">
+        <h1>Welcome to Miami!</h1>
+        <h2>Just kidding... to RPS!</h2>
+        <div class="login">
+            <p>Do your thing:</p>
+            <a href="login.php"><span>Please Log In</span></a>
+        </divi>
+        <h4>if you don't log in or attempt to go to <a href="game.php">game.php</a>, no game :-(</h4>
+    </div>
+</body>
+</html>
