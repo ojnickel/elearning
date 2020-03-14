c304db1878b1f1c5818caa37fbf7612dede8561b
Oswaldo Nickel <oswaldo.nickel@gmail.com>
2020-03-14 01:00:07 +0100

    add validation        in  js     for add..php

--- coursera/wa4e/javascript/assigments/1/add.php
+++ coursera/wa4e/javascript/assigments/1/add.php
@@ -40,6 +40,34 @@ if (isPost()) {
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/style.css" media="all">
     <title>Oswaldo  Nickel's new in Garage</title>
+    <script>
+        function doValidateForm() {
+            console.log("Validating...");
+            try {
+                fn = document.getElementById("id_fn").value;
+                ln = document.getElementById("id_ln").value;
+                he = document.getElementById("id_he").value;
+                su = document.getElementById("id_su").value;
+                em = document.getElementById("id_em").value;
+                console.log("Validating fn=" + fn + " ln=" + ln + " em=" + em + " he=" + he + " su=" + su
+);
+                if ((em == null || em == "") || (fn == null || fn == "") || (he == null || he == "") || (ln == null || ln == "") || (su == null || su == ""))
+{
+                    alert("All fields are required");
+                    return false;
+                }
+                console.log("Validating em=" + em);
+                if   (em.indexOf("@")<0){
+                    alert("Email address must contain @");
+                    return false;
+                }
+                return true;
+            } catch (e) {
+                return false;
+            }
+            return false;
+        }
+    </script>
 </head>
 <body>
     <div id="main">
@@ -53,18 +81,18 @@ if (isPost()) {
         } ?>
             <form action="" method="post">
                 <label class="lbl" for="">first_name</label>
-                <input id="" type="text" name="first_name">
+                <input id="id_fn" type="text" name="first_name">
                 <label class="lbl" for="">last_name</label>
-                <input id="" type="text" name="last_name">
+                <input id="id_ln" type="text" name="last_name">
                 <label class="lbl" for="">email</label>
-                <input id="" type="text" name="email">
+                <input id="id_em" type="text" name="email">
                 <label class="lbl" for="">headline</label>
-                <input id="" type="text" name="headline">
+                <input id="id_he" type="text" name="headline">
                 <label class="lbl" for="">Summary</label>
-                <textarea id="" name="summary" cols="30" rows="10"></textarea>
+                <textarea id="id_su" name="summary" cols="30" rows="10"></textarea>
                 <div class="lbl">
-                    <input class="link" type="submit" value="Add" name="add">
-                    <input class="link" type="submit" value="Cancel" name="cancel">
+                    <input class="btn" type="submit" onclick="return doValidateForm();" value="Add" name="add">
+                    <input class="btn" type="submit" value="Cancel" name="cancel">
                 </div>
             </form>
         </div>    
