7b4d3975c27fca420acb83c3d718261d143873d7
Oswaldo Nickel <oswaldo.nickel@gmail.com>
2020-03-14 01:04:51 +0100

    add validation js -> edit.php

--- coursera/wa4e/javascript/assigments/1/edit.php
+++ coursera/wa4e/javascript/assigments/1/edit.php
@@ -44,6 +44,34 @@ missing();
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/style.css" media="all">
     <title>Oswaldo Nickel's edit garage</title>
+    <script>
+        function doValidateForm() {
+            console.log("Validating...");
+            try {
+                fn = document.getElementById("id_fn").value;
+                ln = document.getElementById("id_ln").value;
+                he = document.getElementById("id_he").value;
+                su = document.getElementById("id_su").value;
+                em = document.getElementById("id_em").value;
+                console.log("Validating fn=" + fn + " ln=" + ln + " em=" + em + " he=" + he + " su=" + su
+);
+                if ((em == null || em == "") || (fn == null || fn == "") || (he == null || he == "") || (ln == null || ln == "") || (su == null || su == ""))
+{
+                    alert("All fields are required");
+                    return false;
+                }
+                console.log("Validating em=" + em);
+                if   (em.indexOf("@")<0){
+                    alert("Email address must contain @");
+                    return false;
+                }
+                return true;
+            } catch (e) {
+                return false;
+            }
+            return false;
+        }
+    </script>
 </head>
 <body>
     <div id="main">
@@ -57,28 +85,28 @@ missing();
         } ?>
             <form action="" method="post">
                 <label class="lbl" for="">first_name</label>
-                <input id="" type="text" name="first_name" value="<?= htmlentities(
+                <input id="id_fn" type="text" name="first_name" value="<?= htmlentities(
                     $row['first_name']
                 ) ?>">
                 <label class="lbl" for="">last_name</label>
-                <input id="" type="text" name="last_name" value="<?= htmlentities(
+                <input id="id_ln" type="text" name="last_name" value="<?= htmlentities(
                     $row['last_name']
                 ) ?>">
                 <label class="lbl" for="">email</label>
-                <input id="" type="text" name="email" value="<?= htmlentities(
+                <input id="id_em" type="text" name="email" value="<?= htmlentities(
                     $row['email']
                 ) ?>">
                 <label class="lbl" for="">headline</label>
-                <input id="" type="text" name="headline" value="<?= htmlentities(
+                <input id="id_he" type="text" name="headline" value="<?= htmlentities(
                     $row['headline']
                 ) ?>">
                 <label class="lbl" for="">Summary</label>
-                <textarea id="" name="summary" cols="30" rows="10" ><?= htmlentities(
+                <textarea id="id_su" name="summary" cols="30" rows="10" ><?= htmlentities(
                     $row['summary']
                 ) ?></textarea>
                 <div class="lbl">
-                    <input class="link" type="submit" value="Save" name="add">
-                    <input class="link" type="submit" value="Cancel" name="cancel">
+                    <input class="btn" type="submit" onclick="return doValidateForm();" value="Save" name="add">
+                    <input class="btn" type="submit" value="Cancel" name="cancel">
                 </div>
             </form>
         </div>    
